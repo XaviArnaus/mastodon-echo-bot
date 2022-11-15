e8deda143e1862b15e3f19ad88de2ea29ca312cc
Xavi <xavi@arnaus.net>
2022-11-15 08:07:00 +0100

    Add a way to quickly publish the queue

--- Makefile
+++ Makefile
@@ -13,6 +13,10 @@ run:
 create_app:
 	$(PYTHON) create_app.py
 
+.PHONY: publish_queue
+publish_queue:
+	$(PYTHON) publish_queue.py
+
 .PHONY: validate_config
 validate_config:
 	@$(PYTHON) -c 'import yaml;yaml.safe_load(open("config.yaml"))' > /dev/null && echo "\033[0;32mThe Config is correct\033[0m" || echo "\033[0;31mThe Config has an error\033[0m"
---
+++ publish_queue.py
@@ -0,0 +1,34 @@
+from bundle.logger import Logger
+from bundle.config import Config
+from src.mastodon_helper import MastodonHelper
+from src.publisher import Publisher
+
+class QueuePublisher:
+    '''
+    Main Runner of the Echo bot
+    '''
+    def init(self):
+        self._config = Config()
+        self._logger = Logger(self._config).getLogger()
+
+        return self
+
+    def run(self):
+        '''
+        Just publishes the queue
+        '''
+        try:
+            # All actions are done under a Mastodon API instance
+            mastodon = MastodonHelper.get_instance(self._config)
+
+            # Read from the queue the toots to publish
+            # and publishes all of them
+            publisher = Publisher(self._config, mastodon)
+            self._logger.info("Publishing the whole queue")
+            publisher.publish_all_from_queue()
+        except Exception as e:
+            self._logger.exception(e)
+
+if __name__ == '__main__':
+    QueuePublisher().init().run()
+
