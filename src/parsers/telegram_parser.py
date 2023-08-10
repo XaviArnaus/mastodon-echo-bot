0ffac057384491f25e15a10c3b0b82a2cfbbb881
Xavi <xavi@arnaus.net>
2023-08-10 09:26:30 +0200

    Discovering Entity, getting messages from entity, storing seen messages

--- src/parsers/telegram_parser.py
+++ src/parsers/telegram_parser.py
@@ -1,7 +1,7 @@
 from pyxavi.config import Config
 from pyxavi.storage import Storage
 from telethon import TelegramClient
-from telethon.utils import get_input_channel, get_input_peer
+from telethon.utils import get_input_channel, get_input_peer, resolve_id
 # from telethon.tl.functions.channels import TypeInputChannel
 from telethon.tl.types import InputPeerChannel
 from telethon.tl.types import ChannelMessagesFilter
@@ -17,7 +17,7 @@ class TelegramParser:
         self._config = config
         self._logger = logging.getLogger(config.get("logger.name"))
         self._toots_queue = Storage(self._config.get("toots_queue_storage.file"))
-        self._feeds_storage = Storage(self._config.get("telegram_parser.storage_file"))
+        self._chats_storage = Storage(self._config.get("telegram_parser.storage_file"))
         self._telegram = self.initialize_client()
     
     def telegram_ok(self) -> None:
@@ -42,46 +42,60 @@ class TelegramParser:
         The Telegram wrapper is reactive. You can't parse a list of messages but
         react on an incomming message.
         """
-        channels = self._config.get("telegram_parser.channels", [])
+        # Chats and channels are managed equally, but under different entities.
+        chats = self._config.get("telegram_parser.chats", [])
+        chats += self._config.get("telegram_parser.channels", [])
 
-        if not channels:
-            self._logger.info("No Telegram channels registered to parse, skipping,")
+        if not chats:
+            self._logger.info("No Telegram conversations registered to parse, skipping,")
             return
+        else:
+            # We only need the chat IDs to then retrieve later the Entities.
+            chats = list(filter(bool,[abs(chat["id"]) if "id" in chat else False for chat in chats]))
+        
+        # Get the entities that match with the given IDs.
+        self._logger.info(f"Get matching entities from the current user's dialogs")
+        entities = list(filter(bool,[
+            dialog.entity if dialog.entity.id in chats else False\
+                for dialog in self._telegram.iter_dialogs()
+        ]))
+        dd(entities, max_depth=2)
 
-        for channel in channels:
-            channel_name = channel["name"]
-            channel_id = channel["id"]
+        # If no entities found, return.
+        logger_string = f"Got {len(entities)} entities."
+        if not entities:
+            logger_string += " Returning."
+            self._logger.info(logger_string)
+            return
+        self._logger.info(logger_string)
 
-            self._logger.info(f"Getting possible stored data for {channel_name}")
-            channel_data = self._feeds_storage.get_hashed(str(channel_id), None)
+        # Now work with the messages for each entity
+        for entity in entities:
 
-            # Print all dialog IDs and the title, nicely formatted
-            self._logger.info(f"Print all dialog IDs and the title")
-            for dialog in self._telegram.iter_dialogs():
-                # print('{:>14}: {}'.format(dialog.id, dialog.title))
-                if dialog.id == channel_id:
-                    dd(dialog.title)
+            # We have to control what did we already see, to avoid duplicates
+            seen_message_ids = list(self._chats_storage.get(f"entity_{entity.id}", []))
 
-            # self._logger.info("Get all dialogs of the acount")
-            # dialogs = self._telegram.get_dialogs()
-            # for dialog in dialogs:
-            #     dd(dialog.title)
-
-            # self._logger.info(f"Getting the channel entity for {channel_name}")
-            # entity = self._telegram.get_entity(channel_id)
-            # dd(entity)
-
-            self._logger.info(f"Getting messages from chat {channel_name}")
-            # From most-recent to oldest
-            # "peer_id": (PeerChannel){"channel_id": (int)1668477107}
-            #input_channel = get_input_channel(get_input_peer(channel_id))
+            # Retrieving messages:
+            #   reverse=True -> from oldest to newest, to keep the posting order
+            #   offset_id -> avoid retrieving messages that we already know
+            self._logger.info(f"Getting messages for entity {entity.title}")
             for message in self._telegram.iter_messages(
-                channel_id,
+                entity=entity,
                 reverse=True,
-                #filter=ChannelMessagesFilter()
+                offset_id=max(seen_message_ids) if seen_message_ids else 0
             ):
-                dd(message.id)
-                dd(message.text)
-                dd(message.date)
+                # Theoreticaly we don't need to check again the seen message IDs, but...
+                if message.id not in seen_message_ids:
+                    dd(message.id)
+                    dd(message.text)
+                    dd(message.date)
+
+                    # Remember this message
+                    seen_message_ids.append(message.id)
+
+            # Store the new seen value. In the worst case it is the same as before.
+            self._chats_storage.set(f"entity_{entity.id}", seen_message_ids)
+            self._chats_storage.write_file()
+
         
         self._logger.debug("Done")
